85787e77036195e30edcf79b9d49cb46c02aeaac
gregwhitaker <gwhitake@gmail.com>
2019-12-08 09:23:06 -0800

    Doing work

--- src/main/java/example/etag/service/config/DatabaseConfiguration.java
+++ src/main/java/example/etag/service/config/DatabaseConfiguration.java
@@ -14,6 +14,9 @@ import org.springframework.util.StringUtils;
 
 import javax.sql.DataSource;
 
+/**
+ * Database configuration.
+ */
 @Configuration
 @EnableConfigurationProperties({
         DatabaseSettings.class
--- src/main/java/example/etag/service/config/EtagConfiguration.java
+++ src/main/java/example/etag/service/config/EtagConfiguration.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
+/**
+ * E-Tag filter configuration.
+ */
 @Configuration
 public class EtagConfiguration {
 
